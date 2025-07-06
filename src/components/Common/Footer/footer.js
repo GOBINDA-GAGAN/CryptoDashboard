d777186d099505ea7261f4debdcb0d37b21bf523
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-07-06 22:06:37 +0530

    Update footer.js
    bug fix

--- src/components/Common/Footer/footer.js
+++ src/components/Common/Footer/footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css";
 import FacebookIcon from "@mui/icons-material/Facebook";
-import EmailIcon from "@mui/icons-material/Email";
+// import EmailIcon from "@mui/icons-material/Email";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
 
@@ -19,9 +19,9 @@ function Footer() {
         <a href="https://facebook.com">
           <FacebookIcon className="social-link" />
         </a>
-        <a href="mailto:gobindagagandey@gmail.com">
-          <EmailIcon className="social-link" />
-        </a>
+        // <a href="mailto:gobindagagandey@gmail.com">
+        //   <EmailIcon className="social-link" />
+        // </a>
         <a href="https://www.twitter.com">
           <TwitterIcon className="social-link" />
         </a>
